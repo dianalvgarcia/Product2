de21c2adfa17a7df8c196d83fa31f04e77a3a229
Dianalvgarcia <dianalvgarcia@gmail.com>
2023-11-09 19:08:53 -0600

    Product2

--- index.php
+++ index.php
@@ -4,15 +4,53 @@
     <title>Aplicaciones Web Progresivas</title>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <link rel="stylesheet" type="text/css" href="styles.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+      
 
 </head>
 <body>
-    <button class="button" id="btnEstudiante">Diana Laura Velázquez García</button>
-    <button class="button" id="btnMostrarInfo">Aplicaciones Web Progresivas</button>
-    <button class="button" id="btnMostrarPasos">Primeros pasos para construir una PWA</button>
-    <button class="button" id="btnFastWeb">Aplicaciones web más rápidas</button>
-    <button class="button" id="btnCache">Almacenamiento en caché</button>
+    <h1>
+        Aplicaciones Web Progresivas
+    </h1>
+    <div class="carousel-container">
+        <div class="carousel-slide">
+            <button class="button" id="btnEstudiante">Diana Laura Velázquez García</button>
+            <button class="button" id="btnMostrarInfo">Aplicaciones Web Progresivas</button>
+            <button class="button" id="btnMostrarPasos">Primeros pasos para construir una PWA</button>
+            <button class="button" id="btnFastWeb">Aplicaciones web más rápidas</button>
+            <button class="button" id="btnCache">Almacenamiento en caché</button>
 
+        </div>
+    </div>
+    <button id="prevButton">Anterior</button>
+    <button id="nextButton">Siguiente</button>
+    <script>
+        const slide = document.querySelector('.carousel-slide');
+        const prevButton = document.getElementById('prevButton');
+        const nextButton = document.getElementById('nextButton');
+        let currentPosition = 0;
+
+        prevButton.addEventListener('click', () => {
+            currentPosition -= 50;
+            if (currentPosition < 0) {
+                currentPosition = 0;
+            }
+            updateSlidePosition();
+        });
+
+        nextButton.addEventListener('click', () => {
+            currentPosition += 50;
+            if (currentPosition > 100) {
+                currentPosition = 100;
+            }
+            updateSlidePosition();
+        });
+
+        function updateSlidePosition() {
+            slide.style.transform = `translateX(-${currentPosition}%)`;
+        }
+    </script>
+    
     <div id="InfoEstudiante">
         <h1>
             Diana Laura Velázquez García
@@ -23,6 +61,7 @@
     <div id="infoContainer">
         <!-- Contenido de Aplicaciones Web Progresivas -->
         <h2>¿Qué son las Aplicaciones Web Progresivas (PWA)?</h2>
+        <br>
         <p>Las Aplicaciones Web Progresivas (PWA) son aplicaciones web que ofrecen una experiencia similar a las aplicaciones nativas en dispositivos móviles y de escritorio. Se caracterizan por las siguientes características:</p>
         <ul>
             <li>Trabajan offline o en conexiones de red lentas.</li>
@@ -32,11 +71,13 @@
             <li>Se actualizan automáticamente.</li>
         </ul>
         <p>Las PWA son una solución versátil para ofrecer una experiencia de usuario de alta calidad en la web, independientemente de la plataforma o el dispositivo utilizado.</p>
+        
     </div>
 
     <div id="stepsContainer">
         <!-- Contenido de Primeros pasos para construir una PWA -->
         <h2>Primeros pasos para construir una Aplicación Web Progresiva</h2>
+        <br>
         <ol>
             <li>Definir el propósito y las características de la aplicación.</li>
             <li>Crear un diseño y una experiencia de usuario atractiva y receptiva.</li>
@@ -52,11 +93,13 @@
         <p>A continuación, se debe diseñar una interfaz de usuario atractiva y receptiva que funcione bien en dispositivos móviles y de escritorio. La optimización del rendimiento es crucial, ya que las PWA deben cargar rápidamente, incluso en conexiones de red lentas. Esto se logra mediante el uso de técnicas como el almacenamiento en caché con Service Workers. Además, habilitar el trabajo offline es una característica distintiva de las PWA. </p>
         <p>Los Service Workers permiten a la aplicación funcionar sin conexión, mejorando la accesibilidad en todas las circunstancias. También se debe implementar la funcionalidad de instalación en la pantalla de inicio de los dispositivos, lo que facilita que los usuarios accedan rápidamente a la PWA. </p>
         <p>Por último, la seguridad y la privacidad de los usuarios son esenciales. Es importante garantizar que los datos del usuario estén protegidos y que se respeten sus preferencias de privacidad. La creación de una PWA es un proceso progresivo y continuo, en el que se busca mejorar constantemente la experiencia del usuario y mantener la aplicación actualizada automáticamente.</p>
+        <embed src="/APLICACIONES WEB PROGRESIVAS.pdf" type="application/pdf">
     </div>
 
     <div id="fastWebContainer">
         <!-- Contenido de Aplicaciones web más rápidas -->
         <h2>¿Qué hace que una Aplicación Web sea más rápida?</h2>
+        <br>
         <p>Las Aplicaciones Web más rápidas se destacan por su rendimiento ágil y eficiente. Aquí hay algunas prácticas clave para lograr una mayor velocidad en las aplicaciones web:</p>
         <ul>
             <li><strong>Optimización de imágenes:</strong> Reduzca el tamaño de las imágenes y utilice formatos web modernos como WebP para acelerar la carga de imágenes.</li>
@@ -66,10 +109,12 @@
             <li><strong>Minificación y concatenación:</strong> Minimice y concatene archivos CSS y JavaScript para reducir el número de solicitudes al servidor.</li>
         </ul>
         <p>La velocidad de carga y respuesta es esencial para la retención de usuarios y la satisfacción del cliente en el mundo de las aplicaciones web. Al seguir estas prácticas, puedes asegurarte de que tu aplicación web sea lo más rápida y eficiente posible.</p>
+        <embed src="/PDF's/APLICACIONES WEB PROGRESIVAS RAPIDAS.pdf" type="application/pdf">
     </div>
 
     <div id="cacheContainer">
         <h2>Almacenamiento en Caché en Aplicaciones Web</h2>
+        <br>
         <p>El almacenamiento en caché es una técnica fundamental para mejorar la velocidad y el rendimiento de las aplicaciones web. Permite guardar en memoria temporal una copia de recursos, como archivos CSS, JavaScript e imágenes, en el dispositivo del usuario. Esto evita la necesidad de descargar estos recursos cada vez que el usuario accede a la aplicación, lo que acelera significativamente la carga y la respuesta de la misma.</p>
         <p>Existen varias formas de implementar el almacenamiento en caché en una aplicación web:</p>
         <ul>
@@ -77,6 +122,7 @@
             <li><strong>Service Workers:</strong> Los Service Workers son scripts JavaScript que se ejecutan en segundo plano y permiten un control preciso sobre el almacenamiento en caché. Puedes definir qué recursos se almacenan en caché y cómo se gestionan las actualizaciones de los mismos.</li>
         </ul>
         <p>El almacenamiento en caché es esencial para proporcionar una experiencia rápida y receptiva a los usuarios, especialmente en condiciones de red lentas o intermitentes. Al utilizar esta técnica de manera efectiva, puedes reducir la carga del servidor y mejorar la velocidad de tu aplicación web.</p>
+        <embed src="/PDF's/ALMACENAMIENTO CACHÉ.pdf" type="application/pdf">
     </div>
 
     <script>
@@ -121,6 +167,10 @@
             });
         });
     </script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
 </body>
 </html>
 
