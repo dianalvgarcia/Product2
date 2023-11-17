d32644159219e2da558046d0e18cabacb0a71b77
Dianalvgarcia <dianalvgarcia@gmail.com>
2023-11-16 22:06:53 -0600

    Product2

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Bienvenido, crea tu PWA");
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(3000, () => {
+  console.log('Servidor iniciado en http://localhost:3000');
+});
--- sw.js
+++ sw.js
@@ -23,3 +23,13 @@ self.addEventListener('fetch', event => {
       .then(response => response || fetch(event.request))
   );
 });
+
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(3000, () => {
+  console.log('Servidor iniciado en http://localhost:3000');
+});
